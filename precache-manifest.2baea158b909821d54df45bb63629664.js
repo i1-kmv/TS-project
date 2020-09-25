4600617de34b5ad64e4c02dfb58931afe1a72a48
i1-kmv <i1-kmv@mail.ru>
2020-09-25 14:28:20 +0200

    Updates

---
+++ precache-manifest.2baea158b909821d54df45bb63629664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684f04476a2bf849fe87e9fc3f3a746f",
+    "url": "/TS-project/index.html"
+  },
+  {
+    "revision": "f5ce53487d753a9ea5ce",
+    "url": "/TS-project/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "68fffbab4728b394f216",
+    "url": "/TS-project/static/js/2.d68cc3ad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TS-project/static/js/2.d68cc3ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ce53487d753a9ea5ce",
+    "url": "/TS-project/static/js/main.016020cf.chunk.js"
+  },
+  {
+    "revision": "398a6affec288708881c",
+    "url": "/TS-project/static/js/runtime-main.407714ad.js"
+  }
+]);
